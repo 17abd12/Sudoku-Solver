8f6911bd91489ce67a41f3aba27932f786ee9991
17abd12 <abdullahimran1712@gmail.com>
2023-09-02 20:26:13 +0500

    Making Sudoku for predefined board

--- Gui.py
+++ Gui.py
@@ -1,1 +1,30 @@
-print('Hello')
+import tkinter as tk
+    
+def display_sudoku(matrix1,matrix2,text):
+
+    root = tk.Tk()
+    root.title("Soduku")
+
+    canvas = tk.Canvas(root, width=380, height=450)
+    canvas.pack()
+
+
+    canvas.create_text(180,40,text=text,font=('Arial',15),fill = 'RED')
+
+    cell_width = 40  # Adjust this value to control the cell width
+    cell_height = 40  # Adjust this value to control the cell height
+
+    for i in range(len(matrix1)):
+        for j in range(len(matrix1[i])):
+            x1 = j * cell_width +10
+            y1 = i * cell_height + 70
+            x2 = x1 + cell_width
+            y2 = y1 + cell_height
+
+            canvas.create_rectangle(x1, y1, x2, y2, fill="white")
+            if matrix1[i][j] == matrix2[i][j]:
+                canvas.create_text((x1 + x2) / 2, (y1 + y2) / 2, text=str(matrix1[i][j]), font=("Arial", 15),fill = 'RED')
+            else:
+                canvas.create_text((x1 + x2) / 2, (y1 + y2) / 2, text=str(matrix1[i][j]), font=("Arial", 15),fill = 'GREEN')
+
+    root.mainloop()
--- runners.py
+++ runners.py
@@ -1,1 +1,70 @@
-print('Hello')
+from copy import deepcopy
+from Gui import display_sudoku
+
+    
+board = [
+ ["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+            
+def termination(board):
+    for x in board:
+        for y in x:
+            if y == ".":
+                return False
+    else:
+        return True
+def action(board,i,j):
+    result = [str(x) for x in range(1,10)]
+    if board[i][j] != '.':
+        return []
+    for x in range(0,9):
+        # for checking the whole row
+        if board[i][x] != '.':
+            if board[i][x] in result:
+                result.remove(board[i][x])
+        # for checking the whole column
+        if board[x][j] != '.':
+            if board[x][j] in result:
+                result.remove(board[x][j])
+        # for checking other half
+    z,s = (i // 3) * 3,(j // 3) * 3
+    for r in range(z,z + 3):
+            for q in range(s,s + 3):
+                if board[r][q] != '.':
+                    if board[r][q] in result:
+                        result.remove(board[r][q])         
+    return result
+
+def recursive(board):
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if board[i][j] == '.':
+                possible_actions = action(board,i,j)
+                for x in possible_actions:
+
+                    result = False
+                    board[i][j] = x
+                    if termination(board):
+                        print('The Board Has Ended')
+                        return True
+                    else:
+                       result =  recursive(board)
+
+                    if result:
+                        return True
+                else:
+                    print('No Such solution')
+                    board[i][j] = '.'
+                    return False
+
+display_sudoku(board,board,'Sudoku Unsolved')
+b = deepcopy(board)
+recursive(board)
+display_sudoku(board,b,'Soduko Solved')
